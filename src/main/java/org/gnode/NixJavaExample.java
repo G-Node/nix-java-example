b4031e257b00d999b09aca38a28f070581c96b56
Christian Kellner <christian@kellner.me>
2016-02-27 01:02:34 +0100

    Initial commit a bare minimal working example

---
+++ src/main/java/org/gnode/NixJavaExample.java
@@ -0,0 +1,23 @@
+package org.gnode;
+
+import org.g_node.nix.*;
+
+public class NixJavaExample {
+    public static void main(String[] args) {
+        System.out.println("Writing to file");
+
+        File fd = File.open("test.nix", FileMode.Overwrite);
+
+        Block b = fd.createBlock("Data", "example");
+        DataArray d = b.createDataArray("fibonacci", "numbers", DataType.Double, new NDSize(2, 3));
+
+        float[] data = {1, 1, 2, 3, 5, 8};
+        d.setData(data, new NDSize(2, 3), new NDSize(0, 0));
+
+        d = null;
+        b = null;
+
+        fd.close();
+        System.out.println("Done.");
+    }
+}
