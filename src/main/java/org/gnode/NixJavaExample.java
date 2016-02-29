260762febb5ffe04b3ae7aefc1a35f5d30c36be1
Christian Kellner <christian@kellner.me>
2016-02-29 23:01:17 +0100

    Use int arrays to specify shape in NDSize

--- src/main/java/org/gnode/NixJavaExample.java
+++ src/main/java/org/gnode/NixJavaExample.java
@@ -9,10 +9,10 @@ public class NixJavaExample {
         File fd = File.open("test.nix", FileMode.Overwrite);
 
         Block b = fd.createBlock("Data", "example");
-        DataArray d = b.createDataArray("fibonacci", "numbers", DataType.Double, new NDSize(2, 3));
+        DataArray d = b.createDataArray("fibonacci", "numbers", DataType.Double, new NDSize(new int[]{2, 3}));
 
         float[] data = {1, 1, 2, 3, 5, 8};
-        d.setData(data, new NDSize(2, 3), new NDSize(0, 0));
+        d.setData(data, new NDSize(new int[]{2, 3}), new NDSize(2, 0));
 
         d = null;
         b = null;
